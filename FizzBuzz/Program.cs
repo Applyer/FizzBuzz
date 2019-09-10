c56def4957ab17d9f1c906c236c434bcfbd2f58d
evoltech-khallouf <khallouf-a@evoltech.co.jp>
2019-09-10 17:54:21 +0900

    細かな調整

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -13,7 +13,6 @@ namespace FizzBuzz
         public const int Buzznum      = 5;       // Buzzの数値
         public const int Maxnum       = 100;     // 最高値
 
-        public const string Nothing   = "";      // 何もない字
         public const string Fizz      = "Fizz";  // Fizzの文字
         public const string Buzz      = "Buzz";  // Buzzの文字
     }
@@ -25,21 +24,23 @@ namespace FizzBuzz
             // 現在値が最高値になるまでループ
             for(int currentnum = 1; currentnum <= Invariant.Maxnum; currentnum++)
             {
-                // 現在値が1でなければ改行
-                if (currentnum != 1)
-                    Console.WriteLine(Invariant.Nothing);
+                // 現在値を3で割る
+                var isFizz = currentnum % Invariant.Fizznum == 0;
 
-                // 現在値が3の倍数ならFizzを表示
-                if (currentnum % Invariant.Fizznum == 0)
-                    Console.Write(Invariant.Fizz);
+                // 現在値を5で割る
+                var isBuzz = currentnum % Invariant.Buzznum == 0;
 
-                // 現在値が3の倍数 又 5倍数ではない場合スキップ
-                if (currentnum % Invariant.Fizznum == 0 && currentnum % Invariant.Buzznum != 0)
-                    continue;
+                // 3の倍数ならFizzを表示
+                if (isFizz) Console.Write(Invariant.Fizz);
 
-                // 現在値が5の倍数ならBuzzを表示
-                // それ以外の場合現在値の数値を表示
-                Console.Write(currentnum % Invariant.Buzznum == 0 ? Invariant.Buzz : currentnum.ToString());
+                // 5の倍数ならBuzzを表示
+                if (isBuzz) Console.Write(Invariant.Buzz);
+
+                // どちらでもない場合現在値を表示
+                if (!isFizz && !isBuzz) Console.Write(currentnum);
+
+                // 改行
+                Console.WriteLine();
             }
             // 画面閉じ防止
             Console.ReadLine();
